daebdcf2990e28808813745d0c887c2d69bad521
zerotwo <zerotwobotondiscord@gmail.com>
2021-12-30 20:26:56 +0100

    hmM

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-if (process.argv.includes("-devenv"))
-  require("dotenv").config({ path: "./dev.env" });
-else require("dotenv").config();
+if (process.argv.includes('-devenv'))
+  require('dotenv').config({ path: './dev.env' });
+else require('dotenv').config();
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-let fastify = require("fastify");
+let fastify = require('fastify');
 
 fastify = fastify({
   logger: process.env.USE_FASTIFY_LOGGER || false,
   trustProxy: !!process.env.TRUST_PROXY,
 });
 
-const ROUTE_PATHS = ["client", "server", "account"];
+const ROUTE_PATHS = ['client', 'server', 'account'];
 
 for (let routePath of ROUTE_PATHS) {
   for (let file of fs.readdirSync(routePath)) {
-    if (file.endsWith(".js")) {
+    if (file.endsWith('.js')) {
       console.log(`Registering routes from file ${path.join(routePath, file)}`);
       fastify.register(require(path.join(__dirname, routePath, file)));
     }
@@ -27,7 +27,7 @@ async function start() {
   try {
     await fastify.listen(
       process.env.LISTEN_PORT || 80,
-      process.env.LISTEN_IP || "0.0.0.0"
+      process.env.LISTEN_IP || '0.0.0.0'
     );
   } catch (ex) {
     console.error(ex);
